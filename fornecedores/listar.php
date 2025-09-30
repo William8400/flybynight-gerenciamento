0a10c00c811ab35ccefa51e956558155d28698a4
William8400 <wdonato02@gmail.com>
2025-09-30 14:27:56 -0300

    UPTADE

---
+++ fornecedores/listar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Listas Fornecedores </title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+
+<body>
+
+    <h1>Fornecedores</h1>
+    <a href="">Novo fornecedor</a>
+    <a href="../index.php">Voltar</a>
+
+
+
+</body>
+
+</html>
