655bebaf97dc02e52d74a9c4a7a7945a195d4408
William8400 <wdonato02@gmail.com>
2025-10-09 14:54:19 -0300

    Adicionando apelidos nas colunas

--- produtos/listar.php
+++ produtos/listar.php
@@ -1,3 +1,14 @@
+<?php
+
+require_once "../src/produto_crud.php";
+
+$produtos = buscarProdutos($conexao);
+
+echo"<pre>";
+var_dump($produtos);
+echo"</pre>";
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
