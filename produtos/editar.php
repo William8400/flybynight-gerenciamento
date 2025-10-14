a85920ea34600e5021b33da1dd93a4456df9ced6
William8400 <wdonato02@gmail.com>
2025-10-14 14:07:25 -0300

    UPTADE
    copiando as divs do arquivo inserir e colando no editar

--- produtos/editar.php
+++ produtos/editar.php
@@ -1,53 +1,58 @@
-<?php
-require_once "../src/fornecedor_crud.php";
-
-// Pegando da url o valor do parâmetro chamado id
-$id = $_GET['id'];
-
-// Chamamos a função, passando dados de conexão e o id do fornecedor a ser buscado
-$fornecedor = buscarFornecedorPorid($conexao, $id);
-
-if( $_SERVER['REQUEST_METHOD'] === 'POST'){
-    $nome = $_POST['nome'];
-    atualizarFornecedor($conexao, $nome, $id);
-
-    // Após redirecionar usando header()...
-    header("location:listar.php");
-
-    // ... sempre encerre/interrompa o script (evitando erros/execuções adicionais)
-    exit;
-}
-
-// o comando <pre> organiza o array na tela
-/* echo "<pre>";
-var_dump($fornecedor);
-echo "</pre"; */
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Fornecedor</title>
+    <title>Editar produto</title>
     <link rel="stylesheet" href="../css/estilos.css">
 </head>
+
 <body>
-    
-<h1>Editar fornecedor</h1>
 
-<form action="" method="post">
-    <!-- Sempre coloque o código/id do registro de forma oculta no formulário -->
-    <input type="hidden" name="id" value="<?=$fornecedor['id']?>">
-    <div>
-        <label for="nome">Nome:</label>
-        <input value="<?=$fornecedor['nome']?>" type="text" name="nome" id="nome" required>
-    </div>
-    <button type="submit">Atualizar</button>
-</form>
+    <h1>Editar produto</h1>
 
-<a href="listar.php">👈 Voltar</a>
+    <form action="" method="post">
+        <!-- Sempre coloque o código/id do registro de forma oculta no formulário -->
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" required>
+        </div>
+        <div>
+            <label for="descricao">Descrição:</label>
+            <textarea name="descricao" id="descricao" rows="6"></textarea>
+        </div>
+        <div>
+            <label for="preco">preço:</label>
+            <input type="number" name="preco" id="preco" require min="0" step="0.01">
+
+        </div>
+        <div>
+            <label for="quantidade">quantidade:</label>
+            <input type="number" name="quantidade" id="quantidade" require min="0">
+        </div>
+        <div>
+            <label for="fornecedor">Fornecedor:</label>
+            <select name="fornecedor" id="fornecedor">
+                <!-- Sempre mantenha um option vazio.
+             É o usuário que deve vir aqui escolher.-->
+                <option value=""></option>
+
+                <?php foreach ($fornecedores as $fornecedor): ?>
+
+                    <option value="<?= $fornecedor['id'] ?>"><?= $fornecedor['nome'] ?> <?= $fornecedor['id'] ?></option>
+
+                <?php endforeach; ?>
+
+            </select>
+        </div>
+        <button type="submit">Atualizar</button>
+    </form>
+
+    <a href="listar.php">👈 Voltar</a>
 
 
 
 </body>
+
 </html>
