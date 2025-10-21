7b1682daa1bc7d91b4758ae865ce64046713b78d
William8400 <wdonato02@gmail.com>
2025-10-21 14:02:24 -0300

    .

--- src/estoque_crud.php
+++ src/estoque_crud.php
@@ -3,10 +3,9 @@ require_once '../conecta.php';
 
 function buscarlojas_produtos($conexao){
     $sql = "SELECT 
-    
             lojas_id, lojas.nome AS nome_loja,
             produto_id, produtos.nome AS nome_produto, 
-            estoque FROM lojas_produtos JOIN lojas ON lojas.id = nome_lojas.id
+            estoque FROM lojas_produtos JOIN lojas ON lojas.id = produtos.fornecedor_id
              ";
 }
 
